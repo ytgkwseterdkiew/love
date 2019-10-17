3eff92d2b63c7ac2b61f147bc87023b1a6943e2e
ytgkwseterdkiew <56524642+ytgkwseterdkiew@users.noreply.github.com>
2019-10-17 17:12:07 +0800

    Create 3.php

---
+++ 201910/3.php
@@ -0,0 +1,13 @@
+{hkcms: sql="SELECT * FROM hkcms_article WHERE status=99 ORDER BY inputtime DESC" page="$page" num="5"}
+{:dump($data)}
+{/hkcms:get)
+
+{hkcms:get table="hkcms_article" status="99" order="inputtime DESC" page="$page" num"5"
+循环列表，默认返回数据是￥data
+<ul>
+{volist name="data" id="vo"}
+<li>标题:{$vo.title},地址:{$vo.url}<li>
+{/volist}
+</ul>
+分页:{$pages}
+{/hkcms:get}
