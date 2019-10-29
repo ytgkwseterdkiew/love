e3b5fd7ebf2c79d5adfe0cdb46294d5e05a3694e
ytgkwseterdkiew <56524642+ytgkwseterdkiew@users.noreply.github.com>
2019-10-29 10:04:16 +0800

    Create 20191029.php

---
+++ 201910/20191029.php
@@ -0,0 +1,86 @@
+ <style type="text/css">
+       .main-promote .col-6{
+        background: #dedede;
+       }
+       .main-promote .cat{
+        background: #6e6568;
+        color: #fff;
+       }
+       .main-promote .cat .item{
+        padding: 16px 20px;
+       }
+       .main-promote .cat .item:hover{
+        background:rgba(0,0,0,0.2);
+       }
+       .main-promote .info{
+        padding: 10px;
+        background: #fff;
+        color: #888;
+        box-shadow: 0 1px 2px rgba(0,0,0,0.1);
+       }
+       .main-promote .info .avatar{
+        background: #aaa;
+        width: 50px;
+        height: 50px;
+        float: left;
+        border-radius: 50%;
+        margin-right: 10px;
+       }
+       .col-2,
+       .col-5,
+       .col-6{
+        float: left;
+        display: block;
+        position: relative;
+        min-height: 1px;
+       }
+       .col-2{
+        width: 20%;
+       }
+       .col-5{
+        width: 50%;
+       }
+       .col-6{
+        width: 60%;
+       }
+    </style>
+    <div class="main-promote clearfix">
+    <div class="container">
+        <div class="col-2 cat">
+            <div class="item">1</div>
+            <div class="item">2</div>
+            <div class="item">3</div>
+            <div class="item">4</div>
+            <div class="item">5</div>
+            <div class="item">6</div>
+            <div class="item">7</div>
+            <div class="item">8</div>
+            <div class="item">9</div>
+        </div>
+        <div class="col-6">
+            <div class="silder">
+                <img src="images/slider01.png" alt="轮播图">
+            </div>
+            <div class="sub-promote">
+                <div class="col-5">
+                    <img src="images/001.png" alt="广告图1">
+                </div>
+                <div class="col-5">
+                    <img src="images/002.png" alt="广告图2">
+                </div>
+            </div>
+        </div>
+        <div class="col-2 info">
+            <div class="auth clearfix">
+                <div class="avatar"></div>
+                你好，欢迎剁手~
+            </div>
+            <div class="anno">
+                <div class="title">公告</div>
+                <div class="content">
+                    我是侧边栏
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
